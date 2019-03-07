0631017ad75761b8968248cf0e072c1d70708c2c
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-03-06 21:21:10 -0800

    Sets up proxy

--- server.js
+++ server.js
@@ -1,44 +1,26 @@
-let express = require('express');
-let compress = require('compression');
+const express = require('express');
+const compress = require('compression');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const proxy = require('http-proxy-middleware');
+
 const port = 8000;
 
-let app = express();
-app.use(compress());
-app.use(express.static(__dirname + '/public'));
+const app = express();
 
-let proxy = require('http-proxy-middleware');
-app.use(
-  '/api/reserve',
-  proxy({
-    target: 'http://ec2-18-191-229-0.us-east-2.compute.amazonaws.com',
-    changeOrigin: true
-  })
-);
 app.use(
   '/api/photos',
   proxy({
-    target: 'http://ec2-18-206-121-61.compute-1.amazonaws.com',
-    changeOrigin: true
-  })
-);
-app.use(
-  '/menu',
-  proxy({
-    target: 'http://ec2-3-17-28-103.us-east-2.compute.amazonaws.com/',
-    changeOrigin: true
-  })
-);
-app.use(
-  '/overview',
-  proxy({
-    target: 'http://ec2-18-191-13-163.us-east-2.compute.amazonaws.com/',
+    target: 'http://localhost:8888',
     changeOrigin: true
   })
 );
 
-let morgan = require('morgan');
+
+app.use(compress());
+app.use(express.static(__dirname + '/public'));
+
 app.use(morgan('dev'));
-let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
